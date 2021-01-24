871731ece387455142a47fa81fea8346dc265dd6
Anish Aravind <anish.arvind407@gmail.com>
2021-01-24 22:13:55 +0530

    Added random number generator

---
+++ Cryptography/RandomNumbers/RandomNumberGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+
+namespace Cryptography.RandomNumbers
+{
+    public class RandomNumberGenerator
+    {
+        public static  byte[] GetRandomNumberByLength(int length)
+        {
+            // system.random is predicatable.. this is better but slower 
+            byte[] randomNumber = new byte[length];
+
+            using (var random = new RNGCryptoServiceProvider())
+                random.GetBytes(randomNumber);
+
+            return randomNumber;
+        }
+    }
+}
